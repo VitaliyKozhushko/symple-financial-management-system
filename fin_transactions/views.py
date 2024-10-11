e41a059de4c732b8a90628d2f4d9983812503c50
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-10-11 22:03:12 +0300

    refactoring

--- fin_transactions/views.py
+++ fin_transactions/views.py
@@ -13,6 +13,29 @@ from .models import Transaction
 from .serializers import TransactionsSerializer
 
 
+def validate_amount(data: dict[str, Any]) -> Decimal:
+    """
+        Проверяет, что сумма (amount) является числом и больше нуля.
+        Возвращает значение суммы как Decimal, если проверка пройдена.
+
+        Аргументы:
+            data (dict[str, Any]): данные запроса
+        Исключения:
+            ValidationError: если сумма не является числом или меньше либо равна нулю
+        Возвращает:
+            Decimal: проверенное значение 'amount'
+        """
+    try:
+        amount = Decimal(data.get('amount', '0'))
+    except (ValueError, TypeError, InvalidOperation) as err:
+        raise ValidationError({"amount": "Сумма транзакции должна быть числом."}) from err
+
+    if amount <= 0:
+        raise ValidationError({"amount": "Сумма транзакции должна быть больше нуля"})
+
+    return amount
+
+
 class TransactionListCreateView(generics.ListCreateAPIView):  # type: ignore
     """
     Получение списка транзакций и создание транзакции
@@ -26,12 +49,7 @@ class TransactionListCreateView(generics.ListCreateAPIView):  # type: ignore
 
     @add_bearer_security
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        try:
-            amount = Decimal(request.data.get('amount', '0'))
-        except (ValueError, TypeError, InvalidOperation) as err:
-            raise ValidationError({"amount": "Сумма транзакции должна быть числом."}) from err
-        if amount <= 0:
-            raise ValidationError({"amount": "Сумма транзакции должна быть больше нуля"})
+        validate_amount(request.data)
         return super().post(request, *args, **kwargs)
 
 
@@ -49,12 +67,7 @@ class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):  # type: ign
 
     @add_bearer_security
     def put(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        try:
-            amount = Decimal(request.data.get('amount', '0'))
-        except (ValueError, TypeError, InvalidOperation) as err:
-            raise ValidationError({"amount": "Сумма транзакции должна быть числом."}) from err
-        if amount <= 0:
-            raise ValidationError({"amount": "Сумма транзакции должна быть больше нуля"})
+        validate_amount(request.data)
         return super().put(request, *args, **kwargs)
 
     @add_bearer_security
